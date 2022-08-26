b2746b5c9539c913a3e8cd11b0a373c12233dd84
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-08-26 22:27:31 +0100

    O(n^const)

--- BigO/O_n.py
+++ BigO/O_n.py
@@ -2,5 +2,8 @@ def print_item(n):
     for i in range(n):
         print(i)
 
+    for j in range(n):
+        print(j)
+
 
 print_item(9)
---
+++ BigO/O_n3.py
@@ -0,0 +1,8 @@
+def print_item_1(n):
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                print(i, j, k)
+
+
+print_item_1(9)
