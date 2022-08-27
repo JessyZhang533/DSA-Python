0382162d2478b9d7df1c20f37d6a9316d00ff68e
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-08-27 23:25:21 +0100

    LinkedList print

--- LinkedList/ll-class.py
+++ LinkedList/ll-class.py
@@ -8,14 +8,21 @@ class Node:
 
 # Below is the linked list class
 class LinkedList:
-
-    # constructor
     def __init__(self, value):
+        " Constructor "
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
         self.length = 1
 
+    def print_list(self, value):
+        " Print the items in the linked list one by one "
+        temp = self.head
+        while temp is not None:  # when the list is not none
+            print(temp.value)
+            temp = temp.next
+
 
 my_linked_list = LinkedList(4)
 print(my_linked_list.head.value)
+my_linked_list.print_list(4)
