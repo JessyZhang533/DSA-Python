35b36591d8aaffb0c9328f0876fc10559203d304
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-02 23:12:01 +0100

    DoublyLinkedList pop

--- DoublyLinkedList/dll-class.py
+++ DoublyLinkedList/dll-class.py
@@ -33,7 +33,25 @@ class DoublyLinkedList:
         self.length += 1
         return True
 
+    def pop(self):
+        " Remove an item from the end of the list & return it "
+        if self.head is None:  # Edge case 1: list is none
+            return None
+        temp = self.tail
+        if self.length == 1:  # Edge case 2: (original)list has length of 1. We consider this case because need to point self.head to None.
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None  # Note: Nonetype object has no attribute 'next'. This is why we need  to separate Edge case 2 in an if statement
+            temp.prev = None
+        self.length -= 1
+        return temp  # Node: temp; Value: temp.value
+        # If with else: some do this, some do that;
+        # If without else: all do this, some do additional stuff in the if statement
+
 
 my_doubly_linked_list = DoublyLinkedList(3)
 my_doubly_linked_list.append(6)
+my_doubly_linked_list.pop()
 my_doubly_linked_list.print_list()
