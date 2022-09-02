3e674f8e4fc5cc5e0907dc7ddcd3bdbfa62bd591
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-02 23:19:11 +0100

    DoublyLinkedList prepend

--- DoublyLinkedList/dll-class.py
+++ DoublyLinkedList/dll-class.py
@@ -50,8 +50,23 @@ class DoublyLinkedList:
         # If with else: some do this, some do that;
         # If without else: all do this, some do additional stuff in the if statement
 
+    def prepend(self, value):
+        " Add an item to the start of the list "
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+            self.length += 1
+        return True
+
 
 my_doubly_linked_list = DoublyLinkedList(3)
 my_doubly_linked_list.append(6)
 my_doubly_linked_list.pop()
+my_doubly_linked_list.prepend(8)
 my_doubly_linked_list.print_list()
