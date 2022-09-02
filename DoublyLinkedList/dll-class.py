f3921efbc964a1ed16a68e84e24a30af2aa07851
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-02 22:13:09 +0100

    DoublyLinkedList append

--- DoublyLinkedList/dll-class.py
+++ DoublyLinkedList/dll-class.py
@@ -14,11 +14,26 @@ class DoublyLinkedList:
         self.length = 1
 
     def print_list(self):
+        " Print all values one by one "
         temp = self.head
         while(temp):
             print(temp.value)
             temp = temp.next
 
+    def append(self, value):
+        " Add an item to the end of the list "
+        new_node = Node(value)
+        if self.head is None:  # Comparison to None should be "is None" instead of "== None"
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
+        self.length += 1
+        return True
+
 
 my_doubly_linked_list = DoublyLinkedList(3)
+my_doubly_linked_list.append(6)
 my_doubly_linked_list.print_list()
