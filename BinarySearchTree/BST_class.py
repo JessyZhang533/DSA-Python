445850d799088edbab81291554796553768134b4
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-03 21:48:14 +0100

    BST minimum

--- BinarySearchTree/BST_class.py
+++ BinarySearchTree/BST_class.py
@@ -43,6 +43,12 @@ class BinarySearchTree:
                 temp = temp.right
         return False  # If iterates until temp is None, return False
 
+    def min_value_node(self, current_node):
+        " Find & return the child node of minimum value starting from a random node "
+        while current_node.left:
+            current_node = current_node.left
+        return current_node  # Use current_node.value if wants to return value of node
+
 
 my_tree = BinarySearchTree()
 my_tree.insert(2)
@@ -50,6 +56,7 @@ my_tree.insert(1)
 my_tree.insert(3)
 print(my_tree.root.left.value)  # Should be 1
 print(my_tree.contain(4))  # Returns nothing without 'print'!
+print(my_tree.min_value_node(my_tree.root))
 
 # Conclusion:
 # 1. Use while loop instead of for loop when you don't know the exact number of iterations
