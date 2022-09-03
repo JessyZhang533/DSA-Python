dd64fefc6b9807906a0cd9b5f5f49c9b6e2be539
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-03 21:20:07 +0100

    BST insert

--- BinarySearchTree/BST_class.py
+++ BinarySearchTree/BST_class.py
@@ -7,11 +7,37 @@ class Node:
 
 class BinarySearchTree:
     def __init__(self):  # Here we do not pass argument 'value' so that an empty BST can be created
+        " Constructor "
         self.root = None  # The uppermost node that has no parent node
+
+    def insert(self, value):
+        " Insert an item into the tree "
+        new_node = Node(value)
+        if self.root is None:  # Edge case 1: original tree is empty
+            self.root = new_node
+            return True  # To end the function
+        temp = self.root
+        while True:
+            if new_node.value == temp.value:  # Edge case 2: new value already existing
+                return False
+            if new_node.value < temp.value:  # Go left
+                if temp.left is None:
+                    temp.left = new_node
+                    return True  # To end the function
+                temp = temp.left
+            else:  # Go right
+                if temp.right is None:
+                    temp.right = new_node
+                    return True  # To end the function
+                temp = temp.right
 
 
 my_tree = BinarySearchTree()
-print(my_tree.root)
+my_tree.insert(2)
+my_tree.insert(1)
+my_tree.insert(3)
+print(my_tree.root.left.value)  # Should be 1
 
 # Conclusion:
 # 1. Use while loop instead of for loop when you don't know the exact number of iterations
+# 2. If the function should stop executing under some condition, use 'return ...' instead of an if statement
