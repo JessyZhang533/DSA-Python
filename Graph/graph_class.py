2bf67481c3eb278d05497a7b388f3d5cc898e3b0
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-04 22:13:15 +0100

    Graph remove vertex

--- Graph/graph_class.py
+++ Graph/graph_class.py
@@ -28,7 +28,7 @@ class Graph:
     def remove_edge(self, v1, v2):
         " Disconnect two vertices/Remove the edge between two vertices "
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
-            try:  # try...except ValueError pass: To fix the case where a vertex exists but is not connected, and it is one of v1 or v2 (see example below)
+            try:  # try...except ValueError pass: To fix the case where a vertex exists but is not connected, and an edge including this vertex is removed
                 self.adj_list[v1].remove(v2)  # '.remove()'
                 self.adj_list[v2].remove(v1)
             except ValueError:
@@ -36,14 +36,26 @@ class Graph:
             return True
         return False  # At least one of the vertices is not in the graph
 
+    def remove_vertex(self, vertex):
+        " Remove the given vertex (& all associated edges) "
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)  # Remove all associated edges
+            del self.adj_list[vertex]  # Delete the vertex as a key
+            return True
+        return False
+
 
 my_graph = Graph()
 my_graph.add_vertex('A')
 my_graph.add_vertex('B')
 my_graph.add_vertex('C')
 my_graph.add_edge('A', 'B')
-my_graph.remove_edge('A', 'C')
+my_graph.add_edge('A', 'C')
+my_graph.add_edge('C', 'B')
+my_graph.remove_vertex('C')
 my_graph.print_graph()
 
 # Conclusions:
 # 1. Two methods for the list: '.append()', '.remove()'
+# 2. Delete an object: 'del' https://www.programiz.com/python-programming/del
