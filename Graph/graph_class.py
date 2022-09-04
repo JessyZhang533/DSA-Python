04b2d82273b828b8b40e54d1b811ec462bc1e966
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-04 21:49:32 +0100

    Graph remove edge

--- Graph/graph_class.py
+++ Graph/graph_class.py
@@ -25,9 +25,25 @@ class Graph:
             return True
         return False  # At least one of the vertices is not in the graph
 
+    def remove_edge(self, v1, v2):
+        " Disconnect two vertices/Remove the edge between two vertices "
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            try:  # try...except ValueError pass: To fix the case where a vertex exists but is not connected, and it is one of v1 or v2 (see example below)
+                self.adj_list[v1].remove(v2)  # '.remove()'
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass  # Ignore the error and move on to return True
+            return True
+        return False  # At least one of the vertices is not in the graph
+
 
 my_graph = Graph()
 my_graph.add_vertex('A')
 my_graph.add_vertex('B')
+my_graph.add_vertex('C')
 my_graph.add_edge('A', 'B')
+my_graph.remove_edge('A', 'C')
 my_graph.print_graph()
+
+# Conclusions:
+# 1. Two methods for the list: '.append()', '.remove()'
