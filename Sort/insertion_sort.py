48417ca232ffa44de123f4b8e6a16e894198cd57
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-05 00:17:38 +0100

    Big O of sorting algorithms

--- Sort/insertion_sort.py
+++ Sort/insertion_sort.py
@@ -3,7 +3,7 @@ def insertion_sort_1(my_list):  # my version
     for i in range(1, len(my_list)):
         temp = my_list[i]
         for j in range(i-1, -1, -1):  # cannot reach -1, can only decrease to 0
-            if temp < my_list[j]:
+            if temp < my_list[j]:  # If list is almost sorted, t.c.=O(n^2) still
                 my_list[j + 1] = my_list[j]
                 my_list[j] = temp
     return my_list
@@ -13,7 +13,7 @@ def insertion_sort_2(my_list):  # udemy version
     for i in range(1, len(my_list)):
         temp = my_list[i]
         j = i - 1
-        while temp < my_list[j] and j > -1:
+        while temp < my_list[j] and j > -1:  # If list is almost sorted, t.c.= O(n)
             my_list[j + 1] = my_list[j]
             my_list[j] = temp
             j -= 1
@@ -23,3 +23,6 @@ def insertion_sort_2(my_list):  # udemy version
 my_list = [19, 8, 10, 7, 1, 2]
 print(insertion_sort_1(my_list))
 print(insertion_sort_2(my_list))
+
+# t.c.=O(n^2) (almost sorted case already commented)
+# s.c.=O(1)
