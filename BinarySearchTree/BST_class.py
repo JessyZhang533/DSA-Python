e981f5e63ce631045160092effe785576a1e0690
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-05 15:05:48 +0100

    Breadth First Search

--- BinarySearchTree/BST_class.py
+++ BinarySearchTree/BST_class.py
@@ -49,6 +49,21 @@ class BinarySearchTree:
             current_node = current_node.left
         return current_node  # Use current_node.value if wants to return value of node
 
+    def BFS(self):
+        " Breadth First Search: traverse nodes row by row, store their values in a list & return the list "
+        current_node = self.root
+        queue = []
+        result_list = []
+        queue.append(current_node)
+        while len(queue) > 0:
+            current_node = queue.pop(0)  # Clear the items in 'queue' one by one
+            result_list.append(current_node.value)  # We need the VALUE not the nodes in the result
+            if current_node.left:  # Add nodes of the next row to 'queue'
+                queue.append(current_node.left)
+            if current_node.right:  # Add nodes of the next row to 'queue'
+                queue.append(current_node.right)
+        return result_list
+
 
 my_tree = BinarySearchTree()
 my_tree.insert(2)
@@ -57,8 +72,10 @@ my_tree.insert(3)
 print(my_tree.root.left.value)  # Should be 1
 print(my_tree.contain(4))  # Returns nothing without 'print'!
 print(my_tree.min_value_node(my_tree.root))
+print(my_tree.BFS())
 
 # Conclusion:
 # 1. Use while loop instead of for loop when you don't know the exact number of iterations
 # 2. If the function/method should stop executing under some condition, use 'return ...' instead of an if statement
 # 3. If there's only 'return...'as the output of a function/method, we need to use 'print' to visualise the output
+# 4. Pop method for lists: '.pop(index)'
