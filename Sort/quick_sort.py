2c1240981f47fe13f4826526b5c3a19cd3cf5039
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-05 13:02:26 +0100

    quick sort

--- Sort/quick_sort.py
+++ Sort/quick_sort.py
@@ -6,7 +6,7 @@ def swap(my_list, index1, index2):
 
 
 def pivot(my_list, pivot_index, end_index):
-    " Group the items smaller than pivot on its left & items greater than pivot on its right "
+    " Group the items smaller than pivot on its left & items greater than pivot on its right; return the pivot index "
     swap_index = pivot_index  # Here we store the index instead of the value
     for i in range(pivot_index+1, end_index+1):
         if my_list[i] < my_list[pivot_index]:
@@ -16,6 +16,16 @@ def pivot(my_list, pivot_index, end_index):
     return swap_index
 
 
+def quick_sort(my_list, left, right):
+    " Recursively find pivot and regroup the list until the list is sorted "
+    if left < right:
+        pivot_index = pivot(my_list, left, right)
+        quick_sort(my_list, left, pivot_index-1)
+        quick_sort(my_list, pivot_index+1, right)
+    return my_list
+
+
 my_list = [5, 3, 9, 1, 6, 7, 8, 0]
 print(pivot(my_list, 0, 7))
 print(my_list)
+print(quick_sort(my_list, 0, 7))
