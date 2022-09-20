d26e72d41cff6ef79b8813914523040f33e525d8
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-20 22:36:43 +0100

    permutation

---
+++ permutations.py
@@ -0,0 +1,25 @@
+# Return all permutations of a list.
+# 1. Make a copy of a list:
+# (1) Shallow copy: the copied list is not the same thing as the original one, but their elements are the exact same things. eg..copy(), [:]
+# (2) Deep copy: the copied list is not the same thing as the original one, elements are not the same thing
+# https://stackoverflow.com/questions/17246693/what-is-the-difference-between-shallow-copy-deepcopy-and-normal-assignment-oper
+# 2. Two main steps of the permutation algorithm: first pop, then append back
+
+def permutation(list):
+    " Return all permutations of a list "
+    result = []
+    # base case
+    if len(list) == 1:
+        return [list[:]]
+    for _ in range(len(list)):
+        n = list.pop(0)  # Here the poped element, n, used to be the first of the list
+        perms = permutation(list)
+        for perm in perms:
+            perm.append(n)
+        result.extend(perms)
+        list.append(n)  # Now we add n back, it has become the last of the list
+    return result
+
+
+my_list = [1, 2, 3, 4]
+print(permutation(my_list))
