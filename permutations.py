e4baf8c32d35eb491b4e941b5e79933b87cf6e09
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-20 23:01:18 +0100

    comments

--- permutations.py
+++ permutations.py
@@ -4,7 +4,7 @@
 # (2) Deep copy: the copied list is not the same thing as the original one, elements are not the same thing
 # https://stackoverflow.com/questions/17246693/what-is-the-difference-between-shallow-copy-deepcopy-and-normal-assignment-oper
 # 2. Two main steps of the permutation algorithm: first pop, then append back
-
+# 3. Add multiple items to a list: .extend()
 def permutation(list):
     " Return all permutations of a list "
     result = []
@@ -16,10 +16,10 @@ def permutation(list):
         perms = permutation(list)
         for perm in perms:
             perm.append(n)
-        result.extend(perms)
+        result.extend(perms)  # 'result' of the first (bottom) call stack of 'permutation' function will always be updated with more and more final permutations
         list.append(n)  # Now we add n back, it has become the last of the list
     return result
 
 
-my_list = [1, 2, 3, 4]
+my_list = [1, 2, 3]
 print(permutation(my_list))
