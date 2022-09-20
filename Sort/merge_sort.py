fcb2d9df936bb1c268be1336cf2ed90b44ec1e67
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-20 17:10:15 +0100

    comment

--- Sort/merge_sort.py
+++ Sort/merge_sort.py
@@ -2,7 +2,7 @@
 # 1. Break the list in half using recursion --> time complexity O(logn)
 # 2. Base case: len(smaller_list) == 1
 # 3. use merge() to combine --> time complexity O(n)
-# spce complexity: O(n)
+# space complexity: O(n)
 # time complexity: O(nlogn)
 
 
