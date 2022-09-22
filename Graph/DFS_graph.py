dc6c7a14c8fcf29718b63c2abd16297eb87f876d
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-22 23:12:18 +0100

    dfs graph

---
+++ Graph/DFS_graph.py
@@ -0,0 +1,29 @@
+# Print/Visit every vertices of the graph; traverse the graph by going to the end of one path at a time
+# In this algorithm, we will be using the concept of 'Stack'
+# t.c.: O(V+E)
+# s.c.: O(V)
+# Here we implement the graph using adjacent list (a dic)
+# https://www.youtube.com/watch?v=tWVWeAqZ0WU&t=1348s
+
+
+nodes_visited = []  # keep track of what nodes are visited
+
+
+def dfs_graph(graph, node):
+    " Print/Visit every vertices of the graph; traverse the graph by going to the end of one path at a time "
+    if node not in nodes_visited:
+        print(node)
+        nodes_visited.append(node)
+        for adj_node in graph[node]:
+            dfs_graph(graph, adj_node)
+
+
+my_graph = {
+  '5': ['3', '7'],
+  '3': ['2', '4'],
+  '7': ['8'],
+  '2': [],
+  '4': ['8'],
+  '8': []
+}  # This is a Directed Graph
+dfs_graph(my_graph, '5')
