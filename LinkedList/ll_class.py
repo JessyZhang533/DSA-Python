a0fe8a51d0fd340af4e58ececdd667f0c945b04a
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-25 01:00:56 +0100

    get, set value, insert, remove

--- LinkedList/ll_class.py
+++ LinkedList/ll_class.py
@@ -50,14 +50,13 @@ class LinkedList:
             self.tail = None  # !!!
         return temp  # temp Or temp.value
 
-
     def prepend(self, value):
         " Add an item to the start of the list "
         new_node = Node(value)
         if self.head is None:  # Edge case 1
             self.head = new_node
             self.tail = new_node
-        else:  # !!!
+        else:  # !!! We have no 'return' statement in the 'if' statement; so we need an else' to separate the code
             new_node.next = self.head
             self.head = new_node
         self.length += 1
@@ -75,26 +74,53 @@ class LinkedList:
             self.tail = None
         return temp   # temp Or temp.value
 
-
     def get(self, index):
         " Return the node at the 'index' "
+        if index < 0 or index > (self.length - 1):
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp  # Will be used in 'insert' and 'remove', so must return a node instead of a value
 
-
-    def set_value(self, index, value):
+    def set_value(self, index, value):  # !!! No need to create a new node
         " Change the value of a node at a certain 'index' "
-
+        temp = self.get(index)
+        if temp:
+            temp.value = value  # Set value directly
+            return True
+        return False
 
     def insert(self, index, value):
         " Insert a value in the list at the position 'index' "
-
+        if index < 0 or index> (self.length - 1):
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length - 1:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
 
     def remove(self, index):
-        " Remove the item at the index "
-
+        " Remove the item at the index "  # SEE REQUIREMENT FIRST. No returning anything here
+        if index < 0 or index > (self.length - 1):
+            return False
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        pre = self.get(index - 1)
+        temp = self.get(index)
+        pre.next = temp.next
+        temp.next = None
 
     def reverse(self):
         " Reverse the sequence of the list "
-
 
 
 my_linked_list = LinkedList(4)
