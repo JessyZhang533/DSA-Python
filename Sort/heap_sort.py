7dafa6af4f390d179e806a604cdd69ad07d7caf1
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-11-08 00:21:47 +0000

    heap sort

--- Sort/heap_sort.py
+++ Sort/heap_sort.py
@@ -1,11 +1,9 @@
-def Heap_sort(list):
-    " Sort a list of values from smallest to largest using the concept of heap "
-    list_heap = MinHeap(len(list))
-    list_sorted = []
-    for i in list:  # O(logn)
-        list_heap.insert(i)
-    for _ in range(len(list)):
-        item_removed = list_heap.remove()
-        list_heap.print()
-        list_sorted.append(item_removed)
-    return list_sorted
+from heapq import heappop
+from heapq import heappush
+
+
+def heapsort(iterable):
+    h = []
+    for value in iterable:
+        heappush(h, value)
+    return [heappop(h) for _ in range(len(h))]
